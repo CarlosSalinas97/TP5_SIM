26a8703c77be206e01e57d91c365d92985605a3b
CarlosSalinas97 <carlos.ariel.salinas@hotmail.com>
2022-05-29 18:02:52 -0300

    Se estructura proyecto. Se agregan clases Cliente y Empleado

---
+++ TP5/Clases/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP5.Clases
+{
+    class Cliente
+    {
+        public static string EN_ESPERA = "En espera";
+
+        public static string SIENDO_ATENTIDO = "Siendo atendido";
+
+        public static string LEYENDO = "Leyendo";
+
+        private string estado { get; set; }
+
+        public Cliente(string estado)
+        {
+            this.estado = estado;
+        }
+    }
+}
---
+++ TP5/Clases/Empleado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP5.Clases
+{
+    class Empleado
+    {
+        public static string LIBRE = "Libre";
+
+        public static string OCUPADO = "Ocupado";
+
+        private string estado;
+
+        public Empleado()
+        {
+            this.estado = LIBRE;
+        }
+
+        public void setOcupado()
+        {
+            this.estado = OCUPADO;
+        }
+
+        public void setLibre()
+        {
+            this.estado = LIBRE;
+        }
+
+        public string getEstado()
+        {
+            return this.estado;
+        }
+    }
+}
